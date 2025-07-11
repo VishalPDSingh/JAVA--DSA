f774213de081a5e62cadccb98cc0707ea3ca1498
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-07-11 05:42:57 +0530

    first commit

---
+++ Ch7_Array/BuyAndSell.java
@@ -0,0 +1,26 @@
+package Ch7_Array;
+
+public class BuyAndSell {
+    public static int buyAndSell(int prices[])
+    {
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1; i<prices.length; i++)
+        {
+            if(buyPrice < prices[i])
+            {
+                int profit = prices[i]-buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[]= {7,1,5,3,6,4};
+        System.out.println(buyAndSell(prices));
+    }
+}
