df243d1b1b2f2d6f805067aed6bac637f5758a7b
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-13 21:16:04 +0530

    first commit

---
+++ Ch12_ArrayList/DemoArray.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class DemoArray {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer>list = new ArrayList<Integer>();
+        ArrayList<String>list2 = new ArrayList<>();
+        ArrayList<Boolean>list3 = new ArrayList<Boolean>();
+
+        //add elements
+        list.add(0);
+        list.add(2);
+        list.add(5);
+        System.out.println(list);
+
+        //get elements
+        int ele=list.get(0);
+        System.out.println(ele);
+
+        // add el in b/w
+        list.add(1,1);
+
+        //set elemntes
+        list.set(1, 10);
+
+        //list remove
+        list.remove(2);
+
+        //list size
+        int size = 
+    }
+}
